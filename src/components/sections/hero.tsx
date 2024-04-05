615e0b7115bb971fb6242a16d560699c8634b8c5
WiseMrMusa <abdulkareemusa01@gmail.com>
2024-04-05 02:17:15 +0100

    added an hero page to it

---
+++ src/components/sections/hero.tsx
@@ -0,0 +1,24 @@
+import { Container } from "../container";
+import { FadeIn } from "../uis/FadeIn";
+
+export const Hero = () => {
+    return(
+        <div>
+            <Container className="mt-24 sm:mt-32 md:mt-56">
+                <FadeIn className="max-w-3xl">
+                    <h1 className="font-display text-5xl font-black tracking-tight text-neutral-950 text-balance sm:text-7xl">
+                    COHI, MSSN Lagos State Area Unit
+                    </h1>
+                    {/* <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
+                        Council of Higher Institutions, Muslim Students&#39; society of Nigeria, Lagos State Area Unit.
+                    </h1> */}
+                    <p className="mt-6 text-xl text-neutral-600 text-balance">
+                        We are the community of Muslim Students across 21 Higher Institutions in Lagos State.
+
+
+                    </p>
+                </FadeIn>
+            </Container>
+        </div>
+    )
+}
